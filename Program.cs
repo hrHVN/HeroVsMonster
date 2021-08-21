ddc2c74a4141f4d8bc3658fe58201634764e4585
andreas Nesheim <nesheim123@hotmail.com>
2021-08-21 05:11:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeroVsMonster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
